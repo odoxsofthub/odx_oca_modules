e2e6ca5a6f0fb8af861df1cf92c98f1704f41d80
Murad <you@example.com>
2023-05-11 17:42:17 +0530

    [ADD]lead stage draggable disable

---
+++ odx_disable_lead_draggable/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+{
+    'name': 'Disable Lead Stage Drag & Drop',
+    'version': '16.0.1.0',
+    'summary': """
+       The drag and drop of lead stages can be restricted from the kanban view by enabling the access group for
+        the particular user
+       """,
+    'category': 'crm',
+    'author': 'Odox SoftHub',
+    'description': """The drag and drop of lead stages can be restricted from the kanban
+     view by enabling the access group for the particular user""",
+    'website': 'https://www.odoxsofthub.com',
+    'depends': [
+        'base',
+        'crm'
+    ],
+    'data': [
+        'security/security.xml',
+        'views/crm_view.xml',
+    ],
+    'images': ['static/description/thumbnail.gif'],
+    'installable': True,
+    'application': True,
+}
