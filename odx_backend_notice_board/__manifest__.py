75f14392f046dfba2e29810628741c65d7a5adee
Murad <you@example.com>
2023-10-11 15:51:23 +0530

    [ADD]notice board module

---
+++ odx_backend_notice_board/__manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Backend Notice Board',
+    'version': '15.0',
+    'author': 'Odox SoftHub',
+    'summary': 'A platform for communication and sharing important information.',
+    'sequence': 1,
+    'description': """ The Backend Notice Board module simplifies internal communication, allowing you to 
+    share announcements and messages broadly or to specific groups within your organization, enhancing 
+    communication flexibility.
+     """,
+    'category': 'Services/Tool',
+    'website': 'https://www.odoxsofthub.com',
+    'support': 'support@odoxsofthub.com',
+    'images': [],
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        # Security
+        'security/ir.model.access.csv',
+        # Data
+        'data/data.xml',
+        # views
+        'security/security.xml',
+        'views/odx_notice.xml',
+        'views/configuration.xml',
+        'views/odx_notice_board_view.xml',
+        'views/menus.xml',
+        'wizard/send_notice.xml',
+    ],
+
+    'assets': {
+        'web.assets_backend': [
+            'odx_backend_notice_board/static/src/css/badge.css',
+        ],
+    },
+    'icon': '/odx_backend_notice_board/static/description/icon (1).png',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+    'images': ['static/description/noticeboard.gif'],
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "auto_install": False
+}
---
+++ odx_backend_notice_board/wizard/send_notice.py
@@ -0,0 +1,34 @@
+from odoo import api, fields, models
+
+
+class SendNoticeWizard(models.TransientModel):
+    _name = 'send.notice.wiz'
+    _description = 'Send Notice'
+
+    name = fields.Char(string="name")
+    subject = fields.Char('Subject', default="Latest Article is Here!")
+    notice_content = fields.Html('Content')
+    user_ids = fields.Many2many('res.users', string='Recipient')
+    email_status = fields.Boolean(string='Email Status')
+
+    def action_send_by_mail(self):
+        active_id = self.env['odx.notice.board'].sudo().browse(self._context['active_id'])
+        # if self.email_status == False:
+        partner_ids = False
+        if self.user_ids:
+            partner_ids = self.user_ids.mapped('partner_id')
+        if partner_ids:
+            vals = {
+                'subject': self.subject,
+                'body_html': self.notice_content,
+                'partner_ids': partner_ids,
+                'model': 'odx.notice.board',
+                'recipient_ids': partner_ids,
+                'auto_delete': True,
+                'email_from': self.env.user.partner_id.email if self.env.user else False,
+            }
+
+            mail_sent = self.env['mail.mail'].sudo().create(vals)
+            mail_sent.send()
+        active_id.email_status = True
+
